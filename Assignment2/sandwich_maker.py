cdb24eca5a8ea3feb8a7016d0bfe241834d5d1a3
Talha Khan <tkhan14@charlotte.edu>
2025-02-19 17:54:15 -0500

    Adding sandwich_maker

--- Assignment2/sandwich_maker.py
+++ Assignment2/sandwich_maker.py
@@ -5,7 +5,15 @@ class SandwichMaker:
 
     def check_resources(self, ingredients):
         """Returns True when order can be made, False if ingredients are insufficient."""
-        #####
+        for items,amount in ingredients.items():
+            if self.machine_resources.get(items, 0) < amount:
+                print("Not enough ingredients to make a sandwich")
+                return False
+        return True
+
+
 
     def make_sandwich(self, sandwich_size, order_ingredients):
-        ########
+        for ingredient, amount in order_ingredients.items():
+            self.machine_resources[ingredient] -= amount
+            print(f"Here is the ingredient: {ingredient}")
