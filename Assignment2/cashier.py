09cc681e8c989b2186c723112c157ca749be1b05
Talha Khan <tkhan14@charlotte.edu>
2025-02-19 17:51:25 -0500

    Adding cashier

--- Assignment2/cashier.py
+++ Assignment2/cashier.py
@@ -5,9 +5,22 @@ class Cashier:
     def process_coins(self):
         """Returns the total calculated from coins inserted.
            Hint: include input() function here, e.g. input("how many quarters?: ")"""
-        ###
+        print("Insert Coins.")
+        quarters = int(input("How many quarters?: ")) * 0.25
+        dimes = int(input("How many dimes?: ")) * 0.10
+        nickles = int(input("How many nickles?: ")) * 0.05
+        pennies = int(input("How many pennies?: ")) * 0.01
+        total = quarters + dimes + nickles + pennies
+        return total
+
 
     def transaction_result(self, coins, cost):
         """Return True when the payment is accepted, or False if money is insufficient.
            Hint: use the output of process_coins() function for cost input"""
-        ##
+        if coins >= cost:
+            change = round(cost - coins , 2)
+            print(f"Transaction result is accepted. Your total is: {change}")
+            return True
+        else:
+            print("Transaction result is insufficient.")
+            return False
